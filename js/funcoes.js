93d23a176e364cbb6b4ec2a9b7e900b11de76884
Bruno Roeder <brunoadrielr@gmail.com>
2017-03-04 16:51:26 -0300

    adicionado primeiras regras de click para home, adicionando o evento onKeyPress e ajustando js para ouvir apenas eventos de enter e click

--- js/funcoes.js
+++ js/funcoes.js
@@ -53,27 +53,33 @@ function alterarBorda(e,f) {
     document.getElementById('detalhamentoOA').style.display = 'none';
   }
 
-  function chamarPesquisa() {
-    document.getElementById('fundo_tela').style.display = 'block';
-    document.getElementById('tela_pesquisa').style.display = 'block';
+  function chamarPesquisa(event) {
+    if (event.keyCode == 13 || event.which == 13 || event.type == "click"){
+      document.getElementById('fundo_tela').style.display = 'block';
+      document.getElementById('tela_pesquisa').style.display = 'block';
+    }
   }
   function ocultarPesquisa() {
     document.getElementById('fundo_tela').style.display = 'none';
     document.getElementById('tela_pesquisa').style.display = 'none';
   }
 
-  function chamarLogin() {
-    document.getElementById('fundo_tela').style.display = 'block';
-    document.getElementById('tela_login').style.display = 'block';
+  function chamarLogin(event) {
+    if (event.keyCode == 13 || event.which == 13 || event.type == "click"){
+      document.getElementById('fundo_tela').style.display = 'block';
+      document.getElementById('tela_login').style.display = 'block';
+    }
   }
-  function ocultarLogin() {
+  function ocultarLogin(event) {
     document.getElementById('fundo_tela').style.display = 'none';
     document.getElementById('tela_login').style.display = 'none';
   }
 
-  function chamarCadastro() {
-    document.getElementById('fundo_tela').style.display = 'block';
-    document.getElementById('tela_cadastro').style.display = 'block';
+  function chamarCadastro(event) {
+    if (event.keyCode == 13 || event.which == 13 || event.type == "click"){
+      document.getElementById('fundo_tela').style.display = 'block';
+      document.getElementById('tela_cadastro').style.display = 'block';
+    }
   }
   function ocultarCadastro() {
     document.getElementById('fundo_tela').style.display = 'none';
@@ -232,7 +238,6 @@ function ajustarStatus(posicao) {
     document.getElementById(linha).style.display = display;
   }
 }
-
 
 
 
